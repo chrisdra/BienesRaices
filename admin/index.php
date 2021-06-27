c53f80575d0aec4ba675703779fb4dca4e1bf3ec
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-06-27 19:49:20 -0400

    tabla informativa
    Se crea una seccion de tabla en index para mostrar la informacion de la base de datos.

--- admin/index.php
+++ admin/index.php
@@ -15,6 +15,31 @@
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva Propiedad</a>
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>Casa en la playa</td>
+                    <td><img src="/imagenes/09b65cc11e3d4a322c6242ef40f1bebd.jpg" class="imagen-tabla"></td>
+                    <td>$1200000</td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </main>
 
 <?php 
