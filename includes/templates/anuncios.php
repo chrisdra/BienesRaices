102eccb8ed8d58a7fb2050711263aca85c2227c5
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-04 21:42:41 -0400

    anuncio.php
    Se crea anuncio.php en donde se crea el codigo para poder obtener la informacion directamente desde la base de datos y mostrarla en la pestaña de inde.php

---
+++ includes/templates/anuncios.php
@@ -0,0 +1,51 @@
+<?php
+    //Importar la conexion
+    require __DIR__ . '/../config/database.php';
+    $db = conectarDB();
+
+    //Consultar
+    $query = "SELECT * FROM propiedades LIMIT ${limite}";
+
+    //Obtener los resultados
+    $resultado = mysqli_query($db, $query);
+
+?>
+
+<div class="contenedor-anuncios">
+    <?php while($propiedad = mysqli_fetch_assoc($resultado)): ?>
+        <div class="anuncio">
+
+            <img loading="lazy" src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="anuncio">
+        
+            <div class="contenido-anuncio">
+                <h3><?php echo $propiedad['titulo']; ?></h3>
+                <p><?php echo $propiedad['descripcion']; ?></p>
+                <p class="precio"><?php echo $propiedad['precio']; ?></p>
+                <ul class="iconos-caracteristicas">
+                    <li>
+                        <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
+                        <p><?php echo $propiedad['wc']; ?></p>
+                    </li>
+                    <li>
+                        <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
+                        <p><?php echo $propiedad['estacionamiento']; ?></p>
+                    </li>
+                    <li>
+                        <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="habitaciones">
+                        <p><?php echo $propiedad['habitaciones']; ?></p>
+                    </li>
+                </ul>
+
+                <a href="anuncio.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">
+                    Ver Propiedad
+                </a>
+            </div><!--contenido-anuncio-->
+        </div><!--anuncio-->
+    <?php endwhile; ?>
+</div><!--contenido-anuncios-->
+
+<?php
+
+    //Cerrar la conexion  
+        mysqli_close($db);
+?>
