b6a75fef053241ae687ec25d1965b745e0f2aa27
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-04 21:44:51 -0400

    actualizacion index
    Se eliminan los anuncios estaticos para crear la coneccion con templates y los anuncios de la bdd. en donde se coloca un limite de muestra a 3 anuncios.

--- Mismas paginas pero php/index.php
+++ Mismas paginas pero php/index.php
@@ -28,39 +28,11 @@
     <section class="seccion contenedor">
         <h2>Casas y Depas en Venta</h2>
         
-        <div class="contenedor-anuncios">
-            <div class="anuncio">
-                <picture>
-                    <source srcset="build/img/anuncio3.webp" type="image/webp">
-                    <source srcset="build/img/anuncio3.jpg" type="image/jpeg">
-                    <img loading="lazy" src="build/img/anuncio3.jpg" alt="anuncio">
-                </picture>
-    
-                <div class="contenido-anuncio">
-                    <h3>Casa con alberca</h3>
-                    <p>Casa con alberca y acabados de lujo en la ciudad, excelente oportunidad</p>
-                    <p class="precio">$3.000.000</p>
-                    <ul class="iconos-caracteristicas">
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_wc.svg" alt="icono wc">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_estacionamiento.svg" alt="icono estacionamiento">
-                            <p>3</p>
-                        </li>
-                        <li>
-                            <img class="icono" loading="lazy" src="build/img/icono_dormitorio.svg" alt="habitaciones">
-                            <p>4</p>
-                        </li>
-                    </ul>
+        <?php
 
-                    <a href="anuncio.php" class="boton-amarillo-block">
-                        Ver Propiedad
-                    </a>
-                </div><!--contenido-anuncio-->
-            </div><!--anuncio-->
-        </div><!--contenido-anuncios-->
+            $limite = 3;
+            include 'includes/templates/anuncios.php';
+        ?>
 
         <div class="alinear-derecha">
             <a href="anuncios.php" class="boton-verde">Ver Todas</a>
