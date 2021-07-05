18199fed155e7b7f9b09a7490672eb2a79022c9c
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-05 18:28:41 -0400

    Hash a la password
    Se crea una funcion para que la contraseña no sea visible por lo que se crea una funcion para hacerle Hash.

--- Mismas paginas pero php/usuario.php
+++ Mismas paginas pero php/usuario.php
@@ -8,10 +8,13 @@
     $email = "correo@correo.com";
     $password = "123456";
 
-    //Query para crer el usuario
-    $query = " INSERT INTO usuarios (email, password) VALUES ( '${email}', '${password}'); ";
+    //Password encriptado
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
 
-    echo $query;
+    //Query para crer el usuario
+    $query = " INSERT INTO usuarios (email, password) VALUES ( '${email}', '${passwordHash}'); ";
+
+    // echo $query;
 
     //Agregarlo a la base de datos
     mysqli_query($db, $query);
