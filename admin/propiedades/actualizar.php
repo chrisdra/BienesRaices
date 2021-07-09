11c938e493b194d82d2e53b8f5db52cfaec1171b
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-09 00:11:35 -0400

    Actualizacion actualizar
    Se modifica el require y se inserta la nueva funcion para validacion de usuario

--- admin/propiedades/actualizar.php
+++ admin/propiedades/actualizar.php
@@ -1,5 +1,12 @@
 <?php
 
+    require '../../includes/funciones.php';
+    $auth = estaAutenticado();
+
+    if(!$auth) {
+        header('Location: /');
+    }
+
     //Validar la URL por ID valido
     $id = $_GET['id'];
     $id = filter_var($id, FILTER_VALIDATE_INT);
@@ -140,7 +147,6 @@
         }
     }
 
-    require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
 
