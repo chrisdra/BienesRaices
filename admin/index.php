64b34d9b353b60ad4bd3a92c47b25f5abade0dd0
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-08 23:34:05 -0400

    Session en index
    Se realiza una validacion y redireccion para los usuarios validados con el registro

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,11 @@
 <?php 
+    session_start();
+
+    $auth = $_SESSION['login'];
+
+    if(!$auth) {
+        header('Location: /');
+    }
 
     //Importar la conexion
     require '../includes/config/database.php';
