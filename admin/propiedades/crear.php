d1113bd4d00b31231272efb365592ece0d3ccb5c
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-09 00:11:52 -0400

    actualizacion crear.php
    Se modifica el require y se inserta la nueva funcion para validacion de usuario

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,11 @@
 <?php
+    require '../../includes/funciones.php';
+    $auth = estaAutenticado();
+
+    if(!$auth) {
+        header('Location: /');
+    }
+
     //Base de datos
     require '../../includes/config/database.php';
     $db = conectarDB();
@@ -121,7 +128,6 @@
         }
     }
 
-    require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
 
