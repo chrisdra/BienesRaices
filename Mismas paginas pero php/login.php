b43537778d4605c7a7a51e948ca9377d930aa44f
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-10 03:03:28 -0400

    Actualizacion
    Se agrega la ruta del cliente al ingresar la sesion y logearse

--- Mismas paginas pero php/login.php
+++ Mismas paginas pero php/login.php
@@ -44,6 +44,7 @@
                     $_SESSION['usuario'] = $usuario['email'];
                     $_SESSION['login'] = true;
                     
+                    header('Location: /admin');
                 } else {
                     $errores[] = 'El password es incorrecto';
                 }
