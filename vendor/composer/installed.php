e729067522cbae23564e83343e12ddaaa6d41840
Christian Herrera Cabezas <chrisdra.hc@gmail.com>
2021-07-20 15:54:51 -0400

    Carpeta Vendor
    Se crea de manera automatica la carpeta vendor con todas sus funciones despues de configurar el composer de la aplicacion

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7f19a1babf64b462633785e83520d25deed788f5',
+        'name' => 'chris/bienesraices-hc',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'chris/bienesraices-hc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7f19a1babf64b462633785e83520d25deed788f5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
